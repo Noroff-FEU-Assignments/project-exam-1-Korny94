8d2b2298373a84f9a92466c0345523042111352e
Korny94 <karlmagnusnokling@hotmail.no>
2023-05-11 21:42:22 +0200

    update

--- js/blogs.js
+++ js/blogs.js
@@ -6,11 +6,11 @@ let menuCount = 0;
 menuBlogs.onclick = function () {
   menuCount++;
   if (menuCount % 2 == 1) {
-    blogsH1.style.marginLeft = "-250px";
+    // blogsH1.style.marginLeft = "-250px";
     dropMenu.style.display = "flex";
     blogsDiv1.style.width = "50vw";
   } else {
-    blogsH1.style.marginLeft = "0";
+    // blogsH1.style.marginLeft = "0";
     dropMenu.style.display = "none";
     blogsDiv1.style.width = "90vw";
   }
